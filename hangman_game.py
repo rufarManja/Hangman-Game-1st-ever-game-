5df076f227ca48e1bbe315a13022165183ef77dd
RufarManja <rufaroManjala@gmail.com>
2020-04-18 21:56:50 +1200

    adding start of questionnare

---
+++ hangman_game.py
@@ -0,0 +1,27 @@
+# actual procedure
+while True:
+    print("Lets play!")
+    while True: #this is to get the amount of attempts
+        attempts = input("how many incorrect attempts do you want?: ")
+        try:
+            int(attempts)
+        except ValueError:
+            print("sorry "+attempts+" is an invalid input, please input integer between 1 and 20")
+        if int(attempts) > 20 or int(attempts) < 1:
+            print("sorry "+attempts+" is an invalid input, please input integer between 1 and 20")
+        else:
+            attempts = int(attempts)
+            break    
+    while True: #asking for the length of the word
+        wordlen = input("how many characters long do you want the word to be?: ")
+        try:
+            wordlen(attempts)
+        except ValueError:
+            print("sorry "+worldlen+" is an invalid input, please input integer between 1 and 20")
+        if wordlen(attempts) > 10 or wordlen(attempts) < 4:
+            print("sorry "+worldlen+" is an invalid input, please input integer between 1 and 20")
+        else:
+            wordlen = int(wordlen)
+            break
+
+    #actual procedure of guessing starts here
